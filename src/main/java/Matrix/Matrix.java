0264d57d1d4384d58cf23d246fa70f9938a473e2
megarungle <megarungle@gmail.com>
2020-04-30 00:46:59 +0300

    Initial commit

---
+++ src/main/java/Matrix/Matrix.java
@@ -0,0 +1,8 @@
+package Matrix;
+
+public class Matrix {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Matrix/MatrixTest.java
@@ -0,0 +1,14 @@
+package Matrix;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MatrixTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
